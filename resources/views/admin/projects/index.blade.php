fbd359c5d83627715184bbfe793ffcc17e310dbb
Saimon397 <simonecarrabba@gmail.com>
2023-01-10 20:41:56 +0100

    es.completato_but_style_inguardabile

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,9 +3,9 @@
 <div class="container mt-5">
     <div class="row g-4">
         @foreach ($projects as $project)
-            <div class="col-4">
-                <div>
-                    <img src="https://picsum.photos/id/{{$project->id + 10}}/1920/1080" alt="">
+            <div class="col-4 d-flex justify-content-center align-items-center">
+                <div class="container card-box">
+                    <img class="my-img" src="https://picsum.photos/id/{{$project->id + 10}}/1920/1080" alt="">
                     <div>{{$project->name}}</div>
                     <div>{{$project->diff_lvl}}</div>
                     <div><a href="{{route('admin.projects.show',$project->slug)}}">{{$project->slug}}</a></div>
