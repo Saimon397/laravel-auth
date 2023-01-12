085dad87b00b93a2472201d4473b35f52a7b590f
Saimon397 <simonecarrabba@gmail.com>
2023-01-12 22:02:19 +0100

    mod_views_project-types

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ $type->workflow }}</h1>
-    <ul>
-        @foreach ($type->projects as $project)
-            <li>{{ $project->title }}</li>
-        @endforeach
-    </ul>
+    <div class="text-center mt-5">
+        <h1>{{ $type->workflow }}</h1>
+        <ul>
+            @foreach ($type->projects as $project)
+                <li class="list-unstyled">{{ $project->title }}</li>
+            @endforeach
+        </ul>
+        <button class="btn btn-primary mt-3"><a href="{{ route('admin.types.index') }}"
+                class="text-white">Indietro</a></button>
+    </div>
 @endsection
