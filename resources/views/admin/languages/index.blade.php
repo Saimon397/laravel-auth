38f34e74b0a9ed8954981131fa338a03def1bb32
Saimon397 <simonecarrabba@gmail.com>
2023-01-17 17:39:17 +0100

    fix

--- resources/views/admin/languages/index.blade.php
+++ resources/views/admin/languages/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="">
+    <div id="index-lang">
         <div class="">
             @if (session()->has('message'))
                 <div class="alert alert-success mb-3 mt-3 w-75 m-auto">
